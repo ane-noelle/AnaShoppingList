3bfc9e94e6358c9f0fde44bc8333cb70f66f6145
ane-noelle <anenoelle60@gmail.com>
2025-04-28 11:42:31 -0300

    aaaa

--- AnaShoppingList/app/index.jsx
+++ AnaShoppingList/app/index.jsx
@@ -1,11 +1,25 @@
+import{ Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function Welcome() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.containerImage}>
+        <Image source={require('.../assets/avatar.png')}
+        style={styles.avatar}
+        />
+      </View>
+      <View style={styles.content}>
+       <Text style={styles.title}>Ana Shopping List</Text>
+       <Text style={styles.text}>
+        Monte sua lista de compras e não esqueça mais o que precisa comprar ao sair de casa!!!
+      </Text>
+      <Link style={styles.button} href={"/home"}>
+        <Text style={styles.buttonText}>Acessar</Text>      
+      </Link>
+      </View>
+      <StatusBar style="light" backgroundColor='#000' />
     </View>
   );
 }
@@ -13,8 +27,42 @@ export default function Welcome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#000',
+  },
+  containerImage: {
+    flex: 2,
+    alingItems: 'center',
     justifyContent: 'center',
   },
+  avatar: {
+    width: 230,
+    heigth: 230,
+    resizeMode: 'cover',
+    borderRadius: 115,
+  },
+  content: {
+    flex: 1,
+    backgroundColor:'#dadada',
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingHorizontal: '5%'
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: 28,
+    marginBottom: 20,
+    textAlign: 'center'
+  },
+  text: {
+    fontSize: 16,
+    color: 'grey',
+  },
+  button: {
+    position: 'absolute',
+    backgroundColor: '#000',
+    bottom: 
+  }
+
+  }
 });
